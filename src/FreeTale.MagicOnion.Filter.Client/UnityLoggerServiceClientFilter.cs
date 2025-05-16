30526ff6b52eb27b1c46839b7155b926391865e8
freetale <free@freetale.me>
2025-05-16 23:44:43 +0700

    update info

---
+++ src/FreeTale.MagicOnion.Filter.Client/UnityLoggerServiceClientFilter.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_2017_1_OR_NEWER
+using MagicOnion.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FreeTale.MagicOnion.Filter.Client
+{
+    public class UnityLoggerServiceClientFilter : IClientFilter
+    {
+        public async ValueTask<ResponseContext> SendAsync(RequestContext context, Func<RequestContext, ValueTask<ResponseContext>> next)
+        {
+            ResponseContext response;
+            try
+            {
+                response = await next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                throw;
+            }
+            var status = response.GetStatus();
+            Debug.Log($"{context.MethodPath} {context.MethodPath} -- [{status.StatusCode}] {status}");
+            return response;
+        }
+    }
+}
+#endif
---
+++ src/FreeTale.MagicOnion.Filter.Server/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FreeTale.MagicOnion.Filter.Server
+{
+    public class Class1
+    {
+
+    }
+}
